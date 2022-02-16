038d24805906eafb3db34358b88a240d999cf6cb
Ki11Z0n3 <javimanga93@gmail.com>
2022-02-16 20:42:45 +0100

    Comprobar caracteres del formulario

--- src/Controller/JudgmentController.php
+++ src/Controller/JudgmentController.php
@@ -21,6 +21,29 @@ class JudgmentController extends AbstractController
      */
     public function showForm(Request $request): Response
     {
+        if ($request->request->get('plaintiff') !== null && $request->request->get('defendant') !== null) {
+            return $this->showResult($request);
+        }
         return $this->render('formJudgment.html.twig');
+    }
+
+    private function showResult(Request $request): Response
+    {
+        $plaintiff = array_change_key_case(str_split($request->request->get('plaintiff')), CASE_UPPER);
+        $defendant = array_change_key_case(str_split($request->request->get('defendant')), CASE_UPPER);
+        if (!$this->checkCharacter($plaintiff, $defendant)) {
+            return $this->render('formJudgment.html.twig', ['message' => 'Solo puede contener las letras K, N, V, #.']);
+        }
+    }
+
+    private function checkCharacter($plaintiff, $defendant): bool
+    {
+        $array = array_merge($plaintiff, $defendant);
+        foreach ($array as $value) {
+            if ($value != 'K' && $value != 'N' && $value != 'V' && $value != '#') {
+                return false;
+            }
+        }
+        return true;
     }
 }
